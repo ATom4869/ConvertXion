4a71e205f38278499c7064a3a7e5d7f5b08cd29d
ATom4869 <arsontomanta@gmail.com>
2026-01-24 04:01:12 +0700

    adds monitoring integration to Vercel

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import React from 'react'
-import FileUpload from './components/FileUpload'
+import React from "react";
+import FileUpload from "./components/FileUpload";
+import { Analytics } from "@vercel/analytics/next";
 
 const page = () => {
   return (
-    <div className='mx-auto w-full'>
+    <div className="mx-auto w-full">
+      <Analytics />
       <FileUpload />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
